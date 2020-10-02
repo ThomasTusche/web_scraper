29ca2a4b237c6690b04b8ad6d67fc9d0e61b18c3
Thomas Tusche <t.tusche@hotmail.de>
2020-10-02 15:34:05 +0200

    hopefully final commit

--- web_scraper.py
+++ web_scraper.py
@@ -1,5 +1,3 @@
-import requests
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -11,27 +9,25 @@ DRIVER_PATH = '/usr/bin/chromedriver'
 URL = 'https://www2.hm.com/de_de/home/produkte/kissen.html'
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
 
-options = Options()
+
+### some of those options were recommend in case you receive errors. For me it worked without them but if you face problems,
+### it might help to remove the comments below.
+
+ptions = Options()
 options.add_argument("--no-sandbox")
-options.add_argument("user-agent=[0]".format(user_agent))
-options.add_argument("--window-size=1920,1200")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-extensions")
+#options.add_argument("user-agent=[0]".format(user_agent))
+#options.add_argument("--window-size=1920,1200")
+#options.add_argument("--disable-dev-shm-usage")
+#options.add_argument("--no-sandbox")
+#options.add_argument("--disable-extensions")
 options.add_argument("--headless")
-options.add_argument('--remote-debugging-port=9222')
+#options.add_argument('--remote-debugging-port=9222')
 
 wd = webdriver.Chrome(options=options, executable_path=DRIVER_PATH, service_log_path='/usr/src/app/log.txt')
 
-wd.delete_all_cookies()
+#wd.delete_all_cookies()
+wd.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'})
 
-wd.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like$
-
-
-wd.get("https://www2.hm.com/de_de/home/produkte/kissen.html")
-
-#result.click()
-
-#time.sleep(4)
+wd.get(URL)
 
 print(wd.page_source)
